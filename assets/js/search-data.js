ee1623ac20c5c4337028b77446fc387cacd198ca
aitrics-tom <82203492+aitrics-tom@users.noreply.github.com>
2025-07-02 01:57:57 +0000

    Deploying to gh-pages from @ aitrics-tom/aitrics-tom.github.io@48a78642ff82829f7e239ab3314248f7753a1283 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%74%6F%6D%6D%79%39%37%37%33@%61%69%74%72%69%63%73.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
